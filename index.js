296b3244289be702a4b35d9dd86a87940a584d70
WDgaster66 <50276790+WDgaster66@users.noreply.github.com>
2023-07-21 23:06:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Select the video and the button elements 
-var video = document.getElementById("movie"); 
-var button = document.getElementById("mute-button");
+const video = document.getElementById("movie"); 
+const button = document.getElementById("mute-button");
 
 // Add a click event listener to the button 
 button.addEventListener("click", function() { 
@@ -17,14 +17,15 @@ button.addEventListener("click", function() {
 });
 
 // Select the video, the slider, and the span elements 
-var video = document.getElementById("movie"); 
-var slider = document.getElementById("speed-slider"); 
-var span = document.getElementById("speed-value");
+// Use const for variables that will not change
+const video = document.getElementById("movie"); 
+const button = document.getElementById("mute-button");
+const slider = document.getElementById("speed-slider"); 
+const span = document.getElementById("speed-value");
 
-// Add an input event listener to the slider 
-slider.addEventListener("input", function() { 
-  // Get the value of the slider 
-  var speed = slider.value; 
+// Use let for variables that may change
+let speed = slider.value;
+
   // Set the playback rate of the video to the value of the slider 
   video.playbackRate = speed; 
   // Display the value of the slider in the span element 
