c9fed6fd15fb2a2afdf91c6e0a3e01f8498b6425
WDgaster66 <50276790+WDgaster66@users.noreply.github.com>
2023-07-22 00:06:16 -0700

    Update process.php

--- process.php
+++ process.php
@@ -8,6 +8,6 @@ if (filter_var($email, FILTER_VALIDATE_EMAIL)) { $email = filter_var($email, FIL
 // Trim and escape the other form fields 
 $name = trim($name); $name = htmlspecialchars($name); $source = trim($source); $source = htmlspecialchars($source); $phone = trim($phone); $phone = htmlspecialchars($phone); $address = trim($address); $address = htmlspecialchars($address); $comments = trim($comments); $comments = htmlspecialchars($comments);
 
-// Print out the form data 
+// Print out the form data
 echo “<h1>Thank you for contacting us</h1>”; echo “<p>Your name: $name</p>”; echo “<p>Your email: $email</p>”; echo “<p>Contact reason: $source</p>”; echo “<p>Your phone: $phone</p>”; echo “<p>Your address: $address</p>”; echo “<p>Your comments: $comments</p>”; 
 ?>
